1c85fe9845884472668f2389785b350f2184f26a
hamish liu <hamish.mc.liu@gmail.com>
2022-07-16 17:28:11 +0900

    added update to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,9 @@
 class BookingsController < ApplicationController
+  def update
+    # change the booking details
+    # find the booking
+    booking = Booking.find(params[:id])
+    # Changes the booking params
+    booking.update(booking_params)
+  end
 end
