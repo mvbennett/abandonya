fa47a8498451ff5f1583a1245c77c682493f3a27
mikebennett <mvbennett9@gmail.com>
2022-07-16 15:38:43 +0900

    created new and create actions and house form

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -6,8 +6,22 @@ class HousesController < ApplicationController
   end
 
   def new
+    @user = User.find(current_user.id)
+    @house = House.new
   end
 
   def create
+    @user = User.find(current_user.id)
+    @house = House.new(house_params)
+    @house.user = @user
+    @house.save
+
+    redirect_to house_path(@house)
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:name, :address, :haunted)
   end
 end
