be63abde1638b539c53ce605d5b0e82b26c9a804
mikebennett <mvbennett9@gmail.com>
2022-07-16 15:14:54 +0900

    added routes for houses and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'houses/index'
-  get 'houses/show'
-  get 'houses/new'
-  get 'houses/create'
+  # get 'houses/index'
+  # get 'houses/show'
+  # get 'houses/new'
+  # get 'houses/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :houses, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:index, :new, :create, :edit, :update]
+  end
 end
