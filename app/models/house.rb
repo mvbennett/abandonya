2edf442adbac25d29a03b927339b5189084806f2
mikebennett <mvbennett9@gmail.com>
2022-07-20 20:53:18 +0900

    made basic validations, stringified dates on pages where it will be displayed

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,10 @@
 class House < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  # should ensure number values are 1-5
+  validates :haunted, :inclusion => 1..5
+  validates :name, presence: true
+  validates :address, presence: true
 end
