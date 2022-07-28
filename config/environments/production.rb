bf9ea180fc1c4d309f4c0d2949d982c51fed2767
mikebennett <mvbennett9@gmail.com>
2022-07-28 20:11:28 +0900

    hoping heroku works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.assets.initialize_on_precompile = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
