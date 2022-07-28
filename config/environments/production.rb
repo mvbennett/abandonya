d38a0a8d3712e3e532d5ae3da5892cf3d6d1eaeb
mikebennett <mvbennett9@gmail.com>
2022-07-28 23:18:02 +0900

    removed css asset compressor from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.assets.css_compressor = nil
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
